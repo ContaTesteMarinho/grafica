b3cb145610b2a0515179434d588626ab685166c7
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-03-12 23:40:40 -0300

    Ajustes no endpoint/categorias/id

--- src/main/java/com/feliphe/cursomc/services/CategoriaService.java
+++ src/main/java/com/feliphe/cursomc/services/CategoriaService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.feliphe.cursomc.domain.Categoria;
 import com.feliphe.cursomc.repositories.CategoriaRepository;
+import com.feliphe.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
-	@Autowired /*Automáticamente estânciada pelo spring*/
+	@Autowired
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
