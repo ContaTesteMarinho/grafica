91c9b508ea1bb3fe1096b19c6692b8d60df75321
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-03-17 22:30:39 -0300

    EndPoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/feliphe/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.feliphe.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.feliphe.cursomc.domain.Pedido;
+import com.feliphe.cursomc.repositories.PedidoRepository;
+import com.feliphe.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
