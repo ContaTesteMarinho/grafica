fa4544510901b0ee23e394f8b04f19eec7b2b951
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-04-07 11:49:29 -0300

    inserindo Pedido

--- src/main/java/com/feliphe/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/feliphe/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.feliphe.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.feliphe.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/feliphe/cursomc/services/PedidoService.java
+++ src/main/java/com/feliphe/cursomc/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.feliphe.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.feliphe.cursomc.domain.ItemPedido;
+import com.feliphe.cursomc.domain.PagamentoComBoleto;
 import com.feliphe.cursomc.domain.Pedido;
+import com.feliphe.cursomc.domain.enums.EstadoPagamento;
+import com.feliphe.cursomc.repositories.ItemPedidoRepository;
+import com.feliphe.cursomc.repositories.PagamentoRepository;
 import com.feliphe.cursomc.repositories.PedidoRepository;
 import com.feliphe.cursomc.services.exception.ObjectNotFoundException;
 
@@ -15,6 +21,18 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagtoRepo;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		
 		Optional<Pedido> obj = repo.findById(id);
@@ -22,4 +40,30 @@ public class PedidoService {
 				"Objeto não encontrado! id " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
+	public Pedido insert(Pedido obj) {
+		
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagtoRepo.save(obj.getPagamento());
+		
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		
+		return obj;
+		
+	}
 }
