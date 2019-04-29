d674ec6fb8fdd38fe1c132de0f14cb6b32424505
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-04-28 21:29:18 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/feliphe/cursomc/services/PedidoService.java
+++ src/main/java/com/feliphe/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.feliphe.cursomc.domain.Cliente;
 import com.feliphe.cursomc.domain.ItemPedido;
 import com.feliphe.cursomc.domain.PagamentoComBoleto;
 import com.feliphe.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.feliphe.cursomc.domain.enums.EstadoPagamento;
 import com.feliphe.cursomc.repositories.ItemPedidoRepository;
 import com.feliphe.cursomc.repositories.PagamentoRepository;
 import com.feliphe.cursomc.repositories.PedidoRepository;
+import com.feliphe.cursomc.security.UserSS;
+import com.feliphe.cursomc.services.exception.AuthorizationException;
 import com.feliphe.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -73,5 +79,15 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 		
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
